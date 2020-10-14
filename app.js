9f12ca2c33d812702b0b98db532600842eb37086
onimuraaika <aika.onimura@gmail.com>
2020-10-14 16:41:42 +0900

    [/Add]JavaScriptを 学ぼう6章/クリックイベントを設定する練習

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 $(function(){
-  $('.box1').mouseover(function(){
+  $('.box1').on('click',function(){ //クリックするとグレーに変わる
     $('.box1').addClass('box1-ext');
   });
-  $('.box1').mouseout(function(){
+  $('.box1').mouseout(function(){ //マウスアウトするとピンクに戻る
     $('.box1').removeClass('box1-ext');
   });
 });
+
+//①.click(function)はクリック時の処理を設定
+//②.on('イベント名',function)はイベント発生時に実行する処理を設定
+//①②を組み合わせて、
+//$('セレクタ名').on('click', function(){イベント発生時に実行する処理)}
